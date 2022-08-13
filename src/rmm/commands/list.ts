12a4e5f56fb8f3b2f1c89344da6ec21015ca65c7
GhexterCortes <ghextershumies@gmail.com>
2022-08-14 00:28:56 +0800

    list

--- src/rmm/commands/list.ts
+++ src/rmm/commands/list.ts
@@ -1,8 +1,11 @@
 import { RecipleModulesYml } from '../classes/RecipleModulesYml';
 import { CommandFileParam } from '../types/commands';
 import { program } from '../util/cli';
+import semver from 'semver';
 import yml from 'yaml';
 import { IRecipleModulesYml } from '../types/files';
+import chalk from 'chalk';
+import { toArray } from '../util/converters';
 
 export default (data: CommandFileParam) => program
     .command("list [filter]")
@@ -11,7 +14,16 @@ export default (data: CommandFileParam) => program
     .action(args => {
         const modules: IRecipleModulesYml["modules"] = yml.parse(new RecipleModulesYml().read()).modules;
         
-        for (const _module of modules) {
-            console.log(JSON.stringify(_module));
+        for (const mod of modules) {
+            console.log(`${chalk.blue(mod.name) + chalk.dim('@') + chalk.green(mod.version)} — ${mod.description ?? 'No description.'}`);
+            console.log(`    supported versions: ${toArray(mod.supportedRecipleVersions).map(v => semver.satisfies(`${semver.coerce(data.recipleYml.version)}`, v) ? chalk.green(v) : chalk.dim(v)).join(' ')}`);
+            console.log(`    repository: ${chalk.blue(mod.repositoryURL)}`);
+            console.log(`    folder: ${chalk.blue(mod.containingFolder)}`);
+            console.log(`    tag: ${chalk.blue(mod.tag)}`)
+            console.log(`    files: ${chalk.green('Includes '+ mod.files.length + ' file(s)')}`);
+
+            if (mod.author) console.log(`    author: ${chalk.blue(mod.author)}`);
+            if (mod.license) console.log(`    license: ${chalk.blue(mod.license)}`);
+            if (mod.dependencies) console.log(`    dependencies: ${chalk.blue(Object.keys(mod.dependencies).join(', '))}`);
         }
     });
